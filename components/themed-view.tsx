3312f8a520d1f9a88b8ae2eae21dcb589c18ca13
ake144 <tamiratgedi1@gmail.com>
2025-11-17 20:09:45 +0300

    safe area view

--- components/themed-view.tsx
+++ components/themed-view.tsx
@@ -1,14 +1,26 @@
 import { View, type ViewProps } from 'react-native';
 
 import { useThemeColor } from '@/hooks/use-theme-color';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
+  safe?: boolean;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+export function ThemedView({ style, lightColor,safe=false, darkColor, ...otherProps }: ThemedViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+  const insets = useSafeAreaInsets();
+
+
+  if(safe){
+    return <SafeAreaView style={[{ backgroundColor, flex: 1 }, style]} {...otherProps} />;
+  }
+
+  return <View style={[{ backgroundColor, 
+     paddingBottom: insets.bottom,
+     paddingTop: insets.top
+   }, style]} {...otherProps} />;
 }
